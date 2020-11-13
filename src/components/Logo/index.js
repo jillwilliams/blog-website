c8f371340da805281cff188c5a80d440580f929a
jillwilliams <codeGeek.03.14@gmail.com>
2020-11-12 18:49:14 -0800

    part 1

---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <a href="#">LifeWithNarcolepsy.com</a>
+        </div>
+    );
+}
+
+export default Logo;
---
+++ src/containers/ContactUS/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+const ContactUs = (props) => {
+    return (
+        <div>Contact Us</div>
+    );
+}
+export default ContactUs;
